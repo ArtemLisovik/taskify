c5d0737e19c00192d05379f0f251457f97d0326d
Artem Lisovik <107492333+ArtemLisovik@users.noreply.github.com>
2023-03-11 10:02:51 +0200

    file validation

--- src/containers/WishList/helpers/schema.ts
+++ src/containers/WishList/helpers/schema.ts
@@ -1,15 +1,19 @@
 import * as yup from 'yup'
 
-  export const schema = yup.object().shape({
-    title: yup.string()
-            .required()
-            .min(4, 'Title should be more then 4 letters'),
-    text: yup.string()
-        .required()
-        .min(5,'Desription should be more then 5 letters'),
-    image: yup
-    .mixed()
-    .test("file", "Файл не выбран", (value: any) => {
-      return value && value.length > 0;
-    }),
-  })
+export const schema = yup.object().shape({
+  title: yup.string()
+    .required()
+    .min(4, 'Title should be more then 4 letters'),
+  text: yup.string()
+    .required()
+    .min(5, 'Desription should be more then 5 letters'),
+  image: yup.mixed()
+    .test('noFile', 'Need to upload visualization', (value: any) => {
+      if (!value) return false
+      return true
+    })
+    .test('fileSize', 'Image size cannot be bigger then 10Mb', (value: any) => {
+      return /^image\/(jpe?g|png|gif)$/i.test(value[0].type);
+    })
+
+})
